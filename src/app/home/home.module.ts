60e6938127bcaef1843fd04bf0ff25e8e4d2e01f
AJ Speller <ajspeller@gmail.com>
2022-01-24 20:58:22 -0500

    Add image assets, sqlite, the plugins to the native project and declare components in the home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+
+import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HomePage } from './home.page';
+import { SlideshowPage } from './../slideshow/slideshow.page';
+
+import { DaysAgoPipe } from '../pipes/days-ago.pipe';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, SlideshowPage, DaysAgoPipe],
 })
 export class HomePageModule {}
